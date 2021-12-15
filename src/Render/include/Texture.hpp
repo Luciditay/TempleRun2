fe438a665b33dfc37e4c6cd88c49c7134b10a30f
Yvan <yvan.smorag@orange.fr>
2021-12-15 15:26:59 +0100

    test  terrain

--- src/Render/include/Texture.hpp
+++ src/Render/include/Texture.hpp
@@ -3,4 +3,14 @@
 #include <string>
 #include "GL/glew.h" 
 
+class Texture{
+    public:
+        Texture(std::string pathTexture);
+        void getIdTexture();
+
+    private:
+        std::unique_ptr<Image> m_ptrImage;
+        GLuint m_idTexture;
+};
+
 GLuint loadAndBindTexture(const glimac::Image &ptrImage);
