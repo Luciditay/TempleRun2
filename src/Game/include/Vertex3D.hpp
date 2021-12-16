ed685880c308b1c500cb193eb2e317f40858759d
Yvan <yvan.smorag@orange.fr>
2021-12-16 22:29:54 +0100

    Vertex3D

--- src/Game/include/Vertex3D.hpp
+++ src/Game/include/Vertex3D.hpp
@@ -4,10 +4,12 @@
 
 struct Vertex3DUV{
     glm::vec3 m_position;
-    glm::vec3 m_coordonneesTexture;
+    glm::vec3 m_color;
+    glm::vec2 m_texturePos;
 
-    Vertex3DUV(const glm::vec3 pos, const glm::vec3 texture) {
+    Vertex3DUV(const glm::vec3 pos, const glm::vec3 color, const glm::vec2 texture) {
         m_position = pos;
-        m_coordonneesTexture = texture;
+        m_color = color;
+        m_texturePos = texture;
     }
 };
